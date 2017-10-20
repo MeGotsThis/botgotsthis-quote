183b904abde06ec6c2444c3fc49cc6b52b22997e
MeGotsThis <megotsthis@gmail.com>
2017-10-20 00:53:00 -0700

    Moved database queries to separate methods

--- channel.py
+++ channel.py
@@ -122,6 +122,7 @@ Quotes List: http://megotsthis.com/botgotsthis/t/{args.chat.channel}/quotes''')
     cursor: aioodbc.cursor.Cursor
     query: str
     params: Tuple[Any, ...]
+    result: bool
     quote: str
     id: int
     quoteId: int
@@ -130,76 +131,62 @@ Quotes List: http://megotsthis.com/botgotsthis/t/{args.chat.channel}/quotes''')
             return False
 
         quote = args.message[2:]
-        async with await args.database.cursor() as cursor:
-            try:
-                query = 'INSERT INTO quotes (broadcaster, quote) VALUES (?, ?)'
-                await cursor.execute(query, (args.chat.channel, quote))
-                await cursor.execute('SELECT lastval()')
-                quoteId = int((await cursor.fetchone() or [0])[0])
+        try:
+            quoteId = await library.addQuote(args.database, args.chat.channel,
+                                             args.nick, quote)
 
-                query = '''
-INSERT INTO quotes_history (quoteId, createdTime, broadcaster, quote, editor)
-    VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)
-'''
-                params = quoteId, args.chat.channel, quote, args.nick
-                await cursor.execute(query, params)
-                await args.database.commit()
-
-                args.chat.send(f'''\
+            args.chat.send(f'''\
 Quote has been added for {args.chat.channel} with quote id {quoteId}''')
-            except Exception:
-                bot.utils.logException(timestamp=args.timestamp)
-                args.chat.send('Quote could not have been added')
+            return True
+        except Exception:
+            bot.utils.logException(timestamp=args.timestamp)
+            args.chat.send('Quote could not have been added')
     elif args.message.lower[1] in ['edit', 'update']:
         if len(args.message) < 4:
             return False
 
         quote = args.message[3:]
-        async with await args.database.cursor() as cursor:
-            try:
-                id = int(args.message[2])
-                query = '''
-UPDATE quotes SET quote=? WHERE quoteId=? AND broadcaster=?
-'''
-                await cursor.execute(query, (quote, id, args.chat.channel))
-                if cursor.rowcount == 0:
-                    args.chat.send(f'''\
-Quote id {id} could not been updated. It may not exist.''')
-                    return True
-
-                query = '''
-INSERT INTO quotes_history (quoteId, createdTime, broadcaster, quote, editor)
-    VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)
-'''
-                params = id, args.chat.channel, quote, args.nick
-                await cursor.execute(query, params)
-                await args.database.commit()
-
+        try:
+            id = int(args.message[2])
+        except ValueError:
+            args.chat.send('Quote could not been updated.')
+            return True
+        try:
+            result = await library.updateQuote(
+                args.database, args.chat.channel, args.nick, id, quote)
+            if not result:
                 args.chat.send(f'''\
+Quote id {id} could not been updated. It may not exist.''')
+                return True
+            args.chat.send(f'''\
 Quote id {id} has been update for {args.chat.channel}''')
-            except Exception:
-                bot.utils.logException(timestamp=args.timestamp)
-                args.chat.send('Quote could not been updated.')
+            return True
+        except Exception:
+            bot.utils.logException(timestamp=args.timestamp)
+            args.chat.send('Quote could not been updated.')
     elif args.message.lower[1] in ['del', 'delete', 'rem', 'remove']:
         if len(args.message) < 3:
             return False
 
-        async with await args.database.cursor() as cursor:
-            try:
-                id = int(args.message[2])
-                query = 'DELETE FROM quotes WHERE quoteId=? AND broadcaster=?'
-                await cursor.execute(query, (id, args.chat.channel))
-                await args.database.commit()
-                if cursor.rowcount == 0:
-                    args.chat.send(f'''\
-Quote id {id} could not been deleted. It may not exist.''')
-                    return True
-
+        try:
+            id = int(args.message[2])
+        except ValueError:
+            args.chat.send('Quote could not been deleted.')
+            return True
+        try:
+            result = await library.deleteQuote(args.database,
+                                               args.chat.channel, id)
+            if not result:
                 args.chat.send(f'''\
+Quote id {id} could not been deleted. It may not exist.''')
+                return True
+
+            args.chat.send(f'''\
 Quote id {id} has been deleted for {args.chat.channel}''')
-            except Exception:
-                bot.utils.logException(timestamp=args.timestamp)
-                args.chat.send('Quote could not been deleted.')
+            return True
+        except Exception:
+            bot.utils.logException(timestamp=args.timestamp)
+            args.chat.send('Quote could not been deleted.')
     elif args.message.lower[1] in ['copy']:
         to: str
         if len(args.message) < 3:
@@ -214,136 +201,99 @@ Quote id {id} has been deleted for {args.chat.channel}''')
 I am not in {to} or quotes feature is not enabled in {to}''')
             return True
 
-        async with await args.database.cursor() as cursor:
+        try:
+            id = int(args.message[2])
+        except ValueError:
+            args.chat.send('Quote could not been copied.')
+            return True
+        try:
+            quote = await library.getQuoteById(args.database,
+                                               args.chat.channel, id)
+            if quote is None:
+                args.chat.send(f'''\
+Quote id {id} could not been found. It may not exist.''')
+                return True
+
+            await library.copyQuote(args.database, args.chat.channel, to,
+                                    args.nick, id)
+            args.chat.send(f'''\
+Quote id {id} has been copy for {to} with new id {quoteId}''')
+            return True
+        except Exception:
+            bot.utils.logException(timestamp=args.timestamp)
+            args.chat.send('Quote could not been copied.')
+            return True
+    elif args.message.lower[1] in ['tag', 'tags']:
+        if len(args.message) < 3:
+            return False
+
+        try:
+            id = int(args.message[2])
+        except ValueError:
+            args.chat.send('Quote could not been copied.')
+            return True
+        if len(args.message) >= 4:
             try:
-                id = int(args.message[2])
-                query = '''
-SELECT quote FROM quotes WHERE quoteId=? AND broadcaster=?
-'''
-                await cursor.execute(query, (id, args.chat.channel))
-                quote = ((await cursor.fetchone()) or [None])[0]
+                quoteTags: Tuple[str, ...] = tuple(args.message.lower)[3:]
+                quote = await library.getQuoteById(args.database,
+                                                   args.chat.channel, id)
                 if quote is None:
                     args.chat.send(f'''\
 Quote id {id} could not been found. It may not exist.''')
                     return True
 
-                query = 'INSERT INTO quotes (broadcaster, quote) VALUES (?, ?)'
-                await cursor.execute(query, (to, quote))
-                await cursor.execute('SELECT lastval()')
-                quoteId = int((await cursor.fetchone() or [0])[0])
+                tags: Set[str]
+                tags = await library.getTagsOfQuote(args.database, id)
+                ignoreTags: List[str] = []
+                addTags: List[str] = []
+                deleteTags: List[str] = []
+                tag: str
+                for tag in quoteTags:
+                    if tag[0] in '0123456789':
+                        ignoreTags.append(tag)
+                    if tag in tags:
+                        deleteTags.append(tag)
+                    else:
+                        addTags.append(tag)
+                if addTags:
+                    await library.addTagsToQuote(args.database, id, addTags)
+                if deleteTags:
+                    await library.deleteTagsToQuote(args.database, id,
+                                                    deleteTags)
 
-                query = 'SELECT tag FROM quotes_tags WHERE quoteId=?'
-                quoteTagsParams: List[Tuple[int, str]]
-                quoteTagsParams = [(quoteId, t) async for t,
-                                   in await cursor.execute(query, (id,))]
-                query = 'INSERT INTO quotes_tags (quoteId, tag) VALUES (?, ?)'
-                await cursor.executemany(query, quoteTagsParams)
-
-                query = '''
-INSERT INTO quotes_history  (quoteId, createdTime, broadcaster, quote, editor)
-    VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)
-    '''
-                params = quoteId, to, quote, args.nick
-                await cursor.execute(query, params)
-                await args.database.commit()
-
-                args.chat.send(f'''\
-Quote id {id} has been copy for {to} with new id {quoteId}''')
+                if addTags or deleteTags:
+                    args.chat.send(f'Quote id {id} tags have been updated')
+                    if ignoreTags:
+                        args.chat.send(f'''\
+These tags could not be used: {', '.join(ignoreTags)}''')
+                else:
+                    args.chat.send('No valid tags was specified')
             except Exception:
                 bot.utils.logException(timestamp=args.timestamp)
-                args.chat.send('Quote could not been copied.')
-                raise
-    elif args.message.lower[1] in ['tag', 'tags']:
-        if len(args.message) < 3:
-            return False
-
-        async with await args.database.cursor() as cursor:
-            if len(args.message) >= 4:
-                try:
-                    id = int(args.message[2])
-                    quoteTags: Tuple[str, ...] = tuple(args.message.lower)[3:]
-                    query = '''
-SELECT 1 FROM quotes WHERE quoteId=? AND broadcaster=?
-'''
-                    await cursor.execute(query, (id, args.chat.channel))
-                    quote = await cursor.fetchone()
-                    if quote is None:
-                        args.chat.send(f'''\
+                args.chat.send('Quote tags could not been updated.')
+        else:
+            try:
+                quote = await library.getQuoteById(args.database,
+                                                   args.chat.channel, id)
+                if quote is None:
+                    args.chat.send(f'''\
 Quote id {id} could not been found. It may not exist.''')
-                        return True
-
-                    query = 'SELECT tag FROM quotes_tags WHERE quoteId=?'
-                    tags: Set[str] = {t async for t,
-                                      in await cursor.execute(query, (id,))}
-                    ignoreTags: List[str] = []
-                    addTags: List[Tuple[int, str]] = []
-                    deleteTags: List[Tuple[int, str]] = []
-                    tag: str
-                    for tag in quoteTags:
-                        if tag[0] in '0123456789':
-                            ignoreTags.append(tag)
-                        if tag in tags:
-                            deleteTags.append((id, tag))
-                        else:
-                            addTags.append((id, tag))
-                    if addTags:
-                        query = '''
-INSERT INTO quotes_tags (quoteId, tag) VALUES (?, ?)
-'''
-                        await cursor.executemany(query, addTags)
-                    if deleteTags:
-                        query = '''
-DELETE FROM quotes_tags WHERE quoteId=? AND tag=?
-'''
-                        await cursor.executemany(query, deleteTags)
-                    await args.database.commit()
-
-                    if addTags or deleteTags:
-                        args.chat.send(f'Quote id {id} tags have been updated')
-                        if ignoreTags:
-                            args.chat.send(f'''\
-These tags could not be used: {', '.join(ignoreTags)}''')
-                    else:
-                        args.chat.send('No valid tags was specified')
-                except Exception:
-                    bot.utils.logException(timestamp=args.timestamp)
-                    args.chat.send('Quote tags could not been updated.')
-            else:
-                try:
-                    id = int(args.message[2])
-                    query = '''
-SELECT 1 FROM quotes WHERE quoteId=? AND broadcaster=?'''
-                    await cursor.execute(query, (id, args.chat.channel))
-                    quote = await cursor.fetchone()
-                    if quote is None:
-                        args.chat.send(f'''\
-Quote id {id} could not been found. It may not exist.''')
-                        return True
-                    query = 'SELECT tag FROM quotes_tags WHERE quoteId=?'
-                    tags = {t async for t,
-                            in await cursor.execute(query, (id,))}
-                    if not tags:
-                        args.chat.send('The quote does not have any tags')
-                        return True
-                    args.chat.send(messagesFromItems(tags, 'Tags: '))
-                except Exception:
-                    pass
+                    return True
+                tags: Set[str]
+                tags = await library.getTagsOfQuote(args.database, id)
+                if not tags:
+                    args.chat.send('The quote does not have any tags')
+                    return True
+                args.chat.send(messagesFromItems(tags, 'Tags: '))
+            except Exception:
+                pass
     elif args.message.lower[1] in ['id', 'list']:
         if len(args.message) < 3:
             return False
 
-        async with await args.database.cursor() as cursor:
-            num = len(args.message) - 2
-            query = '''
-SELECT quoteId FROM quotes WHERE broadcaster=? AND quote MATCH ?
-    UNION SELECT quoteId FROM quotes q WHERE broadcaster=? AND
-''' + ' AND '.join(['? IN (SELECT tag FROM quotes_tags t '
-                   'WHERE t.quoteId=q.quoteId)'] * num)
-            query = 'SELECT quoteId FROM (' + query + ') ORDER BY quoteId ASC'
-            params = (args.chat.channel, args.message[2:],
-                      args.chat.channel,) + tuple(args.message.lower)[2:]
-            quoteIds: List[str] = [str(i) async for i,
-                                   in await cursor.execute(query, params)]
+        ids: List[int] = await library.getQuoteIdsByWords(
+            args.database, args.chat.channel, list(args.message)[2:])
+        quoteIds: List[str] = [str(i) for i in ids]
         if not quoteIds:
             args.chat.send('No quotes found with those parameters')
             return True
--- library.py
+++ library.py
@@ -1,4 +1,4 @@
-﻿from typing import Any, Optional, Sequence, Tuple  # noqa: F401
+﻿from typing import Any, List, Optional, Set, Sequence, Tuple  # noqa: F401
 
 import aioodbc.cursor  # noqa: F401
 
@@ -45,7 +45,7 @@ SELECT quote FROM quotes
     WHERE quoteId=(
         SELECT quoteId FROM (%s) ORDER BY RANDOM() LIMIT 1)''' % query
 
-        params: Tuple[Any, ...]
+        params: Tuple[str, ...]
         params = ((channel, ' '.join(words), channel,)
                   + tuple(w.lower() for w in words))
         await cursor.execute(query, params)
@@ -93,7 +93,158 @@ SELECT quote, broadcaster FROM quotes WHERE quoteId=(
     SELECT docid FROM ({query}) ORDER BY RANDOM() LIMIT 1)
 '''
 
-        params: Tuple[Any, ...]
+        params: Tuple[str, ...]
         params = (' '.join(words),) + tuple(w.lower() for w in words)
         await cursor.execute(query, params)
         return await cursor.fetchone() or (None, None)
+
+
+async def addQuote(database: DatabaseMain,
+                   channel: str,
+                   nick: str,
+                   quote: str) -> int:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+INSERT INTO quotes (broadcaster, quote) VALUES (?, ?)
+'''
+        await cursor.execute(query, (channel, quote))
+        await cursor.execute('SELECT lastval()')
+        quoteId: int = int((await cursor.fetchone() or [0])[0])
+
+        query = '''
+INSERT INTO quotes_history (quoteId, createdTime, broadcaster, quote, editor)
+    VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)
+'''
+        await cursor.execute(query, (quoteId, channel, quote, nick))
+        await database.commit()
+        return quoteId
+
+
+async def updateQuote(database: DatabaseMain,
+                      channel: str,
+                      nick: str,
+                      quoteId: int,
+                      quote: str) -> bool:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+UPDATE quotes SET quote=? WHERE quoteId=? AND broadcaster=?
+'''
+        await cursor.execute(query, (quote, quoteId, channel))
+        if cursor.rowcount == 0:
+            return False
+
+        query = '''
+INSERT INTO quotes_history (quoteId, createdTime, broadcaster, quote, editor)
+    VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)
+'''
+        await cursor.execute(query, (quoteId, channel, quote, nick))
+        await database.commit()
+        return True
+
+
+async def deleteQuote(database: DatabaseMain,
+                      channel: str,
+                      quoteId: int) -> bool:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+DELETE FROM quotes WHERE quoteId=? AND broadcaster=?
+'''
+        await cursor.execute(query, (quoteId, channel))
+        await database.commit()
+        return cursor.rowcount != 0
+
+
+async def copyQuote(database: DatabaseMain,
+                    from_channel: str,
+                    to_channel: str,
+                    nick: str,
+                    quoteId: int) -> bool:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+SELECT quote FROM quotes WHERE quoteId=? AND broadcaster=?
+'''
+        await cursor.execute(query, (id, from_channel))
+        quote: Optional[str] = ((await cursor.fetchone()) or [None])[0]
+        if quote is None:
+            return False
+        query = '''
+INSERT INTO quotes (broadcaster, quote) VALUES (?, ?)
+'''
+        await cursor.execute(query, (to_channel, quote))
+        await cursor.execute('SELECT lastval()')
+        quoteId = int((await cursor.fetchone() or [0])[0])
+
+        query = '''
+SELECT tag FROM quotes_tags WHERE quoteId=?
+'''
+        quoteTagsParams: List[Tuple[int, str]]
+        quoteTagsParams = [(quoteId, t) async for t,
+                           in await cursor.execute(query, (id,))]
+        query = '''
+INSERT INTO quotes_tags (quoteId, tag) VALUES (?, ?)
+'''
+        await cursor.executemany(query, quoteTagsParams)
+
+        query = '''
+INSERT INTO quotes_history (quoteId, createdTime, broadcaster, quote, editor)
+VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)
+'''
+        await cursor.execute(query, (quoteId, to_channel, quote, nick))
+        await database.commit()
+        return True
+
+
+async def getTagsOfQuote(database: DatabaseMain,
+                         quoteId: int) -> Set[str]:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+SELECT tag FROM quotes_tags WHERE quoteId=?
+'''
+        return {t async for t, in await cursor.execute(query, (quoteId,))}
+
+
+async def addTagsToQuote(database: DatabaseMain,
+                         quoteId: int,
+                         tags: List[str]) -> bool:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+INSERT INTO quotes_tags (quoteId, tag) VALUES (?, ?)
+'''
+        await cursor.executemany(query, map(lambda t: (quoteId, t), tags))
+        await database.commit()
+        return cursor.rowcount != 0
+
+
+async def deleteTagsToQuote(database: DatabaseMain,
+                            quoteId: int,
+                            tags: List[str]) -> bool:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+DELETE FROM quotes_tags WHERE quoteId=? AND tag=?
+'''
+        await cursor.executemany(query, map(lambda t: (quoteId, t), tags))
+        await database.commit()
+        return cursor.rowcount != 0
+
+
+async def getQuoteIdsByWords(database: DatabaseMain,
+                             channel: str,
+                             words: Sequence[str]) -> List[int]:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+    SELECT quoteId FROM quotes WHERE broadcaster=? AND quote MATCH ?
+        UNION SELECT quoteId FROM quotes q WHERE broadcaster=? AND
+    ''' + ' AND '.join(['? IN (SELECT tag FROM quotes_tags t '
+                        'WHERE t.quoteId=q.quoteId)'] * len(words))
+        query = 'SELECT quoteId FROM (' + query + ') ORDER BY quoteId ASC'
+        params: Tuple[str, ...]
+        params = (channel, ' '.join(words), channel,) + tuple(words)
+        return [i async for i, in await cursor.execute(query, params)]
